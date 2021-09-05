8813cd68c4cfb5ac92aa6f843151848e30127603
RiderWasTaken <andreiammar@gmail.com>
2021-09-06 01:58:20 +0200

    finally done with the 1st part.

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,99 @@
+<div class="footer-top">
+    <div class="link-container">
+        <div class="banners white">
+            <img src="/img/buy-comics-digital-comics.png" alt="">
+            <span>DIGITAL COMICS</span>
+        </div>
+        <div class="banners white">
+            <img src="/img/buy-comics-merchandise.png" alt="">
+            <span>DC MERCHANDISE</span>
+        </div>
+        <div class="banners white">
+            <img src="/img/buy-comics-shop-locator.png" alt="">
+            <span>SUBSCRIPTION</span>
+        </div>
+        <div class="banners white">
+            <img src="/img/buy-comics-subscriptions.png" alt="">
+            <span>COMIC SHOP LOCATOR</span>
+        </div>
+        <div class="banners white">
+            <img src="/img/buy-comics-digital-comics.png" alt="">
+            <span>DC POWER VISA</span>
+        </div>
+    </div>
+</div>
+<div class="footer-mid">
+    <div class="footer-links">
+        <section class="useful-links">
+            <h2 class="white">DC COMICS</h2>
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+            </ul>
+        </section>
+        <section class="useful-links">
+            <h2 class="white">DC</h2>
+            <ul>
+                <li>Terms Of Use</li>
+                <li>Privacy policy (new)</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshop</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+            </ul>
+        </section>
+        <section class="useful-links">
+            <h2 class="white">SITES</h2>
+            <ul>
+    	        <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+            </ul>
+        </section>
+        <section class="useful-links">
+            <h2 class="white">SHOP</h2>
+            <ul>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+            </ul>
+        </section>
+    </div>
+    <div class="footer-dc-logo">
+        <img src="/img/dc-logo-bg.png" alt="">
+    </div>
+</div>
+<div class="footer-bottom">
+    <div class="footer-bottom-left">
+        <button>SIGN-UP NOW!</button>
+    </div>
+    <div class="footer-bottom-right">
+        <button>FOLLOW US</button>
+        <div class="footer-social-button">
+            <img src="/img/footer-facebook.png" alt="">
+        </div>
+        <div class="footer-social-button">
+            <img src="/img/footer-twitter.png" alt="">
+        </div>
+        <div class="footer-social-button">
+            <img src="/img/footer-youtube.png" alt="">
+        </div>
+        <div class="footer-social-button">
+            <img src="/img/footer-pinterest.png" alt="">
+        </div>
+        <div class="footer-social-button">
+            <img src="/img/footer-periscope.png" alt="">
+        </div>
+    </div>
+</div>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,33 @@
+<div class="header-top">
+    <div class="extra-links">
+        <span>DC POWER** VISA**</span>
+        <span>ADDITIONAL DC SITES**</span>
+    </div>
+</div>
+<div class="header-bottom">
+    <div class="nav-container">
+        <div class="top-logo-container">
+            <img src="/img/dc-logo.png" alt="DC Logo">
+        </div>
+        <nav>
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </nav>
+        <div class="top-searchbar">
+            <input type="text" placeholder="Search">
+        </div>
+    </div>
+</div>
+<div class="header-banner">
+
+</div>
